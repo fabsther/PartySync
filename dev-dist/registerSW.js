9166a85a51b943dc82087d592f3409554012294a
fabsther <fabsther@gmail.com>
2025-10-23 18:45:19 +0200

    Updated registerSW.js

--- dev-dist/registerSW.js
+++ dev-dist/registerSW.js
@@ -1,1 +1,7 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+if (import.meta && import.meta.env && import.meta.env.DEV) {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' });
+  }
+} else {
+  // En prod : ne rien faire ici.
+}
